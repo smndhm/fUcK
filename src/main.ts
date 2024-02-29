c78ee3079ab5f3103c99b742a5746db8bc5cdaaf
smndhm <simon.duhem@gmail.com>
2024-02-29 23:06:25 +0100

    feat: random colors

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import './style.css'
 import svg from './svg/border.svg?raw'
 
+  const hue = Math.floor(Math.random() * 360);
+  const saturation = Math.floor(Math.random() * 101);
+  const lightness = Math.floor(Math.random() * 101);
+
+document.documentElement.style.setProperty('--fUcK-background', `hsl(${hue} ${saturation} ${lightness})`);
+document.documentElement.style.setProperty('--fUcK-color', `hsl(${hue} ${saturation} ${lightness+30})`);
+
 document.body.insertAdjacentHTML('afterbegin', svg);
